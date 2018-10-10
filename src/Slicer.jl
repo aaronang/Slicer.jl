fdf8befeeb325502f52514618d263619bbcb1240
Aaron Ang <awz.ang@gmail.com>
2018-10-09 22:08:32 -0700

    Implement STL loading to simple list of triangles

--- src/Slicer.jl
+++ src/Slicer.jl
@@ -1,5 +1,33 @@
 module Slicer
 
-greet() = print("Hello World!")
+struct Vertex
+    x::Float32
+    y::Float32
+    z::Float32
+end
+
+const Vector = Vertex
+
+struct Triangle
+    normal::Vector
+    u::Vertex
+    v::Vertex
+    w::Vertex
+end
+
+function load(stlpath::AbstractString)
+    open(stlpath) do stl
+        skip(stl, 80)  # skip header
+        trianglecount = read(stl, UInt32)
+        ref = Ref{Triangle}()
+        triangles = map(1:trianglecount) do i
+            read!(stl, ref)
+            skip(stl, 2)  # skip attribute byte count
+            ref[]
+        end
+        @assert eof(stl)
+        return triangles
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,6 @@
 using Test
+import Slicer
+
+const STL = joinpath("data", "nist.stl")
+
+@test length(Slicer.load(STL)) == 7392
