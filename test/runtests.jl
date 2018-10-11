53c018dba86f65d6096c303ee689f750fa819447
Aaron Ang <awz.ang@gmail.com>
2018-10-11 09:19:57 -0700

    Add more tests for triangles-plane intersections

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,13 @@ import Slicer: Plane, Vertex, Triangle, AABB, LineSegment
 @testset "Load STL" begin
     stl = joinpath("data", "nist.stl")
     triangles = Slicer.load(stl)
+
     @test length(triangles) == 7392
 end
 
-const triangle1 = Triangle((1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 1))
-const triangle2 = Triangle((1, 0, 0), (0, 0, -.5), (0, 1, -.5), (0, 0, .5))
-const plane = Plane((0, 0, 1), (0, 0, 0))
-
 @testset "Point-plane distance" begin
+    plane = Plane((0, 0, 1), (0, 0, 0))
+
     @test Slicer.distance(Vertex(0, 0, 1), plane) == 1
     @test Slicer.distance(Vertex(1, 1, 1), plane) == 1
     @test Slicer.distance(Vertex(-1, -1, -1), plane) == -1
@@ -20,6 +19,8 @@ const plane = Plane((0, 0, 1), (0, 0, 0))
 end
 
 @testset "Line-plane intersection" begin
+    plane = Plane((0, 0, 1), (0, 0, 0))
+
     @test Slicer.intersect(Vertex(1, 1, 0), Vertex(-1, -1, 0), plane) == [Vertex(1, 1, 0), Vertex(-1, -1, 0)]
     @test Slicer.intersect(Vertex(1, 1, 1), Vertex(-1, -1, -1), plane) == [Vertex(0, 0, 0)]
     @test Slicer.intersect(Vertex(1, 1, 1), Vertex(-1, -1, 1), plane) == []
@@ -27,17 +28,36 @@ end
 end
 
 @testset "Triangle-plane intersection" begin
+    plane = Plane((0, 0, 1), (0, 0, 0))
+    triangle1 = Triangle((1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 1))
+    triangle2 = Triangle((1, 0, 0), (0, 0, -.5), (0, 1, -.5), (0, 0, .5))
+
     @test Slicer.intersect(triangle1, plane) == LineSegment([0, 0, 0], [0, 1, 0])
     @test Slicer.intersect(triangle2, plane) == LineSegment([0, 0, 0], [0, .5, 0])
     @test Slicer.intersect(triangle1, Plane((0, 0, 1), (0, 0, 5))) == nothing
+    @test Slicer.intersect(triangle1, Plane((0, 0, 1), (0, 0, 1))) == nothing
 end
 
 @testset "Triangles-plane intersection" begin
-    @test Slicer.intersect([triangle1, triangle2], plane) ==
+    triangle1 = Triangle((1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 1))
+    triangle2 = Triangle((1, 0, 0), (0, 0, -.5), (0, 1, -.5), (0, 0, .5))
+    triangle3 = Triangle((1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 1, 1))
+
+    @test Slicer.intersect([triangle1, triangle2], Plane((0, 0, 1), (0, 0, 0))) ==
             [LineSegment([0, 0, 0], [0, 1, 0]), LineSegment([0, 0, 0], [0, .5, 0])]
+    @test Slicer.intersect([triangle1, triangle3], Plane((0, 0, 1), (0, 0, 0))) ==
+            [LineSegment([0, 0, 0], [0, 1, 0])]
+    @test Slicer.intersect([triangle1, triangle3], Plane((0, 0, 1), (0, 0, .5))) ==
+            [LineSegment([0, .5, .5], [0, 0, .5]), LineSegment([0, .5, .5], [0, 1, .5])]
+    @test Slicer.intersect([triangle1, triangle3], Plane((0, 0, 1), (0, 0, 1))) ==
+            [LineSegment([0, 1, 1], [0, 0, 1])]
+    @test Slicer.intersect([triangle1, triangle3], Plane((1, 0, 0), (0, 0, 0))) == []
 end
 
 @testset "Axis-aligned bounding box" begin
+    triangle1 = Triangle((1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 1))
+    triangle2 = Triangle((1, 0, 0), (0, 0, -.5), (0, 1, -.5), (0, 0, .5))
+
     @test Slicer.boundingbox(Triangle((1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 1))) == AABB(0, 0, 0, 1, 0, 1)
     @test AABB(0, 0, 0, 1, 0, 42) + AABB(-1, 1, 0, .5, -.5, 1) == AABB(-1, 1, 0, 1, -.5, 42)
     @test Slicer.boundingbox([triangle1, triangle2]) == AABB(0, 0, 0, 1, -.5, 1)
