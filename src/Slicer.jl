666cae127b55e87cf72548d861794e64607753a5
Aaron Ang <awz.ang@gmail.com>
2018-10-15 13:13:36 -0700

    Add simple debugging capability by visualizing polygon in SVG format

--- src/Slicer.jl
+++ src/Slicer.jl
@@ -1,10 +1,21 @@
 module Slicer
 
+using Luxor
+
 include("types.jl")
 include("stl.jl")
 include("intersect.jl")
 include("aabb.jl")
 include("endmatching.jl")
 include("shoelace.jl")
+
+function debug(polygons)
+    @svg begin
+        for polygon in polygons
+            foreach(((a, b),) -> arrow(Point(a[1], a[2]), Point(b[1], b[2]), arrowheadlength=.4, linewidth=.1),
+            edges(polygon))
+        end
+    end
+end
 
 end # module
